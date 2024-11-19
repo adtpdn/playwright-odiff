b119c753771fe9e5d99c079022517c261a5640d9
adtpdn <adtpdn@gmail.com>
2024-11-19 10:58:24 +0800

    increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export const browsers = ["chromium", "firefox", "webkit"] as const;
 
 const config: PlaywrightTestConfig = {
   testDir: "./tests",
-  timeout: 30000,
+  timeout: 60000,
   use: {
     ignoreHTTPSErrors: true,
     screenshot: "only-on-failure",
